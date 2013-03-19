5643f0dc60c3094c17e59523d42ab5a965b86c33
Max De Marzi <maxdemarzi@hotmail.com>
2013-03-18 20:54:19 -0500

    initial commit

---
+++ neo_hubway.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'neography'
+require 'sinatra'
+require 'oj'
+
+def stations
+  neo = Neography::Rest.new
+  cypher = "START start_stations = node:Station(\"stationId:*\") 
+            MATCH start_stations <-[:`START`]- trips -[:END]-> end_stations
+            RETURN start_stations.name, end_stations.name, COUNT(*) AS cnt
+            ORDER BY cnt DESC
+            LIMIT 200"
+            
+  cypher = "start from=node:node_auto_index('*:*') 
+            match from-[r]->to 
+            return from.name, to.name, r.volume"            
+  neo.execute_query(cypher)["data"]            
+end
+
+get '/visualization' do
+  results = stations
+  results.group_by(&:first).map {|k,v| {"name" => k, "follows" => v.collect{|n| { "name" => n[1], "counts" => n[2] }}}}.to_json
+end
+
+get '/' do
+  File.read(File.join('public', 'index.html'))
+end
